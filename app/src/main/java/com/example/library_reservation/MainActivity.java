fd66a2d1ee2246dc24c66016a872c55c759aad09
ehez <ehhernandezh@gmail.com>
2020-09-29 11:48:38 -0400

    Added btn in main

--- app/src/main/java/com/example/library_reservation/MainActivity.java
+++ app/src/main/java/com/example/library_reservation/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.library_reservation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //Erick H
 }
